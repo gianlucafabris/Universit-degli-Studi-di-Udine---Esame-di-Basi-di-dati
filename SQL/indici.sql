997ce1a5496d063bf915b068df0ce23eb6223de4
Gianluca Fabris <79165194+gianlucafabris@users.noreply.github.com>
2025-04-15 22:00:31 +0200

    Add files via upload

---
+++ SQL/indici.sql
@@ -0,0 +1,4 @@
+create index data_visita_storico_indice on Visita_Veterinaria_Storico(data);
+create index data_visita_corrente_indice on Visita_Veterinaria_Corrente(data);
+create index data_entrata_indice on Esemplare_Animale(data_entrata);
+create index data_uscita_indice on Esemplare_Animale(data_uscita);
